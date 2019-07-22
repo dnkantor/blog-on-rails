401ca799cfaa02f9ed9a2537c57d9ba1153c89e6
Daniel Kantor <home@Kantors-MacBook-Pro.local>
2019-07-22 18:50:56 -0400

    Added login and logout buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        #Conditional assignment operator - First check if there is a current user,
+        #if there is no user, it will find it and set it to the current_user variable!
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:danger] = "Must be logged in to perform that action."
+            redirect_to root_path
+        end
+    end
 end
