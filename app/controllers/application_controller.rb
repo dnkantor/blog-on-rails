be67b3613a4c87ededa76a32a3c349c2c4dad25c
Daniel Kantor <home@Kantors-MacBook-Pro.local>
2019-07-24 14:48:56 -0400

    Completed Authentication System

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :logged_in?
+    helper_method :current_user, :logged_in?, :get_username
 
     def current_user
         #Conditional assignment operator - First check if there is a current user,
         #if there is no user, it will find it and set it to the current_user variable!
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def get_username
+        current_user[:username]
     end
 
     def logged_in?
